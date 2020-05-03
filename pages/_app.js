61013b80cfc18f6e49e88dda3de940d5dca5c0ee
Adrien Lav <adrienl@outlook.com>
2020-05-04 00:28:12 +0200

    First

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,63 @@
 import App from 'next/app'
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import 'swiper/css/swiper.css'
 
+const GlobalTheme = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+  }
+  @font-face {
+    font-family: 'Futura';
+    src: url('fonts/Futura-Bold.eot'); /* IE9 Compat Modes */
+    src: url('fonts/Futura-Bold.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('fonts/Futura-Bold.woff2') format('woff2'), /* Super Modern Browsers */
+         url('fonts/Futura-Bold.woff') format('woff'), /* Pretty Modern Browsers */
+         url('fonts/Futura-Bold.ttf')  format('truetype'), /* Safari, Android, iOS */
+         url('fonts/Futura-Bold.svg#svgFontName') format('svg'); /* Legacy iOS */
+    font-weight: 700;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Overpass';
+    src: url('fonts/Overpass-Regular.eot'); /* IE9 Compat Modes */
+    src: url('fonts/Overpass-Regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('fonts/Overpass-Regular.woff2') format('woff2'), /* Super Modern Browsers */
+         url('fonts/Overpass-Regular.woff') format('woff'), /* Pretty Modern Browsers */
+         url('fonts/Overpass-Regular.ttf')  format('truetype'), /* Safari, Android, iOS */
+         url('fonts/Overpass-Regular.svg#svgFontName') format('svg'); /* Legacy iOS */
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Overpass';
+    src: url('fonts/Overpass-Italic.eot'); /* IE9 Compat Modes */
+    src: url('fonts/Overpass-Italic.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('fonts/Overpass-Italic.woff2') format('woff2'), /* Super Modern Browsers */
+         url('fonts/Overpass-Italic.woff') format('woff'), /* Pretty Modern Browsers */
+         url('fonts/Overpass-Italic.ttf')  format('truetype'), /* Safari, Android, iOS */
+         url('fonts/Overpass-Italic.svg#svgFontName') format('svg'); /* Legacy iOS */
+    font-weight: normal;
+    font-style: italic;
+  }
+  @font-face {
+    font-family: 'Overpass';
+    src: url('fonts/Overpass-Black.eot'); /* IE9 Compat Modes */
+    src: url('fonts/Overpass-Black.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('fonts/Overpass-Black.woff2') format('woff2'), /* Super Modern Browsers */
+         url('fonts/Overpass-Black.woff') format('woff'), /* Pretty Modern Browsers */
+         url('fonts/Overpass-Black.ttf')  format('truetype'), /* Safari, Android, iOS */
+         url('fonts/Overpass-Black.svg#svgFontName') format('svg'); /* Legacy iOS */
+    font-weight: 900;
+    font-style: normal;
+  }
+  .swiper-container {
+    height: 100%;
+  }
+`
 const theme = {
   colors: {
     primary: '#0070f3',
@@ -13,6 +69,7 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
+        <GlobalTheme />
         <Component {...pageProps} />
       </ThemeProvider>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,159 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import Swiper from 'react-id-swiper';
+import { years } from './../public/data.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  height: ${props => props.height}px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 `
 
-export default () => <Title>My page</Title>
+const SwiperWrapper = styled.div`
+  flex: 0 1 70%;
+`
+
+const DescriptionWrapper = styled.div`
+  flex: 0 1 30%;
+`
+
+const getBackgroundColor = colors => {
+  if (colors.length > 1) {
+    return `linear-gradient(90deg, #${colors[0].hex}, #${colors[1].hex})`
+  } else {
+    return `#${colors[0].hex}`
+  }
+}
+
+const Slide = styled.div`
+  text-align: center;
+  height: 100%;
+  background: ${props => getBackgroundColor(props.colors)};
+  font-family: Arial;
+  font-weight: 800;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const getFontColor = colors => {
+  return colors[0].hex
+}
+
+const Year = styled.div`
+  font-size: 6rem;
+  font-family: 'Futura';
+  color: #${props => getFontColor(props.colors)};
+  filter: brightness(0.85);
+`
+
+const TitleSection = styled.section`
+  font-family: 'Overpass';
+  padding: 1rem 5vw;
+
+  h2 {
+    font-weight: 700;
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+`
+
+const DescriptionSection = styled.section`
+  font-family: 'Overpass';
+  font-weight: 400;
+  padding: 1rem 5vw;
+  color: #ababab;
+`
+
+const Author = styled.p`
+  font-style: italic;
+  color: black;
+`
+
+export default () => {
+  const [height, setHeight] = useState()
+  const [showDesc, setShowDesc] = useState(false)
+  const [yearsSorted, setYearsSorted] = useState(null)
+  useEffect(() => {
+    const sorted = Array.prototype.slice.call(years).sort((a, b) => a.year > b.year)
+    setYearsSorted(sorted)
+    setIndex(sorted.length - 1)
+    setHeight(window.innerHeight)
+    const sh = height => setHeight(height)
+    window.addEventListener('resize', () => {
+      sh(window.innerHeight)
+    })
+
+    return () => window.removeEventListener('resize', null)
+  }, [])
+
+  const [swiper, setSwiper] = useState(null)
+  const [index, setIndex] = useState(null)
+  useEffect(() => {
+    const setSlideIndex = index => setIndex(index)
+    if (swiper !== null) {
+      swiper.on('slideChange', () => {
+        setSlideIndex(swiper.activeIndex)
+      })
+
+      return () => {
+        swiper.off('slideChange')
+      }
+    }
+  }, [swiper])
+
+  const [code, setCode] = useState("")
+  const [name, setName] = useState("")
+  const [desc, setDesc] = useState("")
+  const [author, setAuthor] = useState("")
+  useEffect( () => {
+    if (yearsSorted) {
+      const getColorCode = year => year.colors.map(entry => entry.code).join(" - ")
+      setCode(getColorCode(yearsSorted[index]))
+
+      const getColorName = year => year.colors.map(entry => entry.name).join(" - ")
+      setName(getColorName(yearsSorted[index]))
+
+      setDesc(yearsSorted[index].description)
+      setAuthor(yearsSorted[index].author)
+    }
+  }, [yearsSorted, index])
+
+  return (
+    <Wrapper height={height}>
+        <SwiperWrapper>
+          {yearsSorted && (
+            <Swiper initialSlide={yearsSorted.length} getSwiper={setSwiper} updateOnWindowResize={true}>
+              {yearsSorted.map(y => {
+                return <Slide 
+                key={y.year}
+                colors={y.colors}>
+                  <Year colors={y.colors}>{y.year}</Year>
+                </Slide>
+              })}
+            </Swiper>
+          )}
+        </SwiperWrapper>
+
+        <DescriptionWrapper onClick={() => setShowDesc(!showDesc)}>
+          <TitleSection>
+            <h2>PANTONE</h2>
+            <p>{code}</p>
+            <p>{name}</p>
+          </TitleSection>
+          {showDesc && (
+            <DescriptionSection>
+              <p>{desc}</p>
+              <Author>{author}</Author>
+            </DescriptionSection>
+          )}
+        </DescriptionWrapper>
+    </Wrapper>
+  )
+}
